6fb06d197d2e29574f8a6314502c872a7013541a
Mohamed Taib <mtaib@c2r2p6.1337.ma>
2023-06-18 20:43:20 +0100

    done

--- microshell.c
+++ microshell.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
-void	 print_error(char *str)
+void	print_error(char *str)
 {
-    int		i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-        write(2, &str[i], 1);
+	i = -1;
+	while (str[++i])
+		write(2, &str[i], 1);
 }
 
-int		count_elements(char **av)
+int	count_elements(char **av)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (av[i] && av[i][0] != '|' && av[i][0] != ';')
@@ -23,9 +23,9 @@ int		count_elements(char **av)
 	return (i);
 }
 
-int		ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (str[i])
@@ -33,7 +33,7 @@ int		ft_strlen(char *str)
 	return (i);
 }
 
-char	*ft_strdup(char	*str)
+char	*ft_strdup(char *str)
 {
 	char	*dupstr;
 	int		i;
@@ -48,12 +48,12 @@ char	*ft_strdup(char	*str)
 		dupstr[i] = str[i];
 		i++;
 	}
-	dupstr[i] ='\0';
+	dupstr[i] = '\0';
 	return (dupstr);
 }
 
-void	reset(int	*prev, int *next, int i, int count)
-{	
+void	reset(int *prev, int *next, int i, int count)
+{
 	if (i > 0 && prev[0])
 	{
 		close(prev[0]);
@@ -65,20 +65,21 @@ void	reset(int	*prev, int *next, int i, int count)
 		prev[1] = next[1];
 	}
 }
-void	exec_cmds(char **av, char **env)
+int		exec_cmds(char **av, char **env)
 {
-	int		i;
-	int		j;
-	int		st;
-	int		count;
-	int		prev[2];
-	int		next[2];
-	int		pid;
+	int	i;
+	int	j;
+	int	st;
+	int	count;
+	int	prev[2];
+	int	next[2];
+	int		exit_status;
 	
+	count = 1;	
+	exit_status = 0;
 	j = 0;
 	st = 0;
 	prev[0] = 0;
-	count = 1;
 	i = 0;
 	while (av[i] && av[i][0] != ';')
 	{
@@ -86,21 +87,39 @@ void	exec_cmds(char **av, char **env)
 			count++;
 		i++;
 	}
+	int	pid[count];
 	if (count == 1 && !strcmp(av[0], "cd"))
 	{
-		if (!av[1])
+		if (!av[1] || (av[1][0] == '|' || av[1][0] == ';'))
+		{
 			print_error("error: cd: bad arguments\n");
-		chdir("..");
-		printf("%s\n",av[1]);
-		return ;
+			return (1);
+		}
+		if (chdir(av[1]))
+		{
+			print_error("error: cd: cannot change directory to path_to_change\n");
+			return (1);
+		}
+		return (0);
 	}
-	i = 0;
-	while (av && *av && (*av)[0] != ';')
-	{	
-		if (j < count - 1);
-			pipe(next);
-		pid = fork();
-		if (pid == 0)
+	i = -1;
+	while (++i < count && av && *av && (*av)[0] != ';')
+	{
+		if (j < count - 1)
+		{
+			if (pipe(next))
+			{
+				print_error("error: fatal\n");
+				exit(1);
+			}
+		}
+		pid[i] = fork();
+		if (pid[i] == -1)
+		{
+			print_error("error: fatal\n");
+			exit(1);
+		}
+		if (pid[i] == 0)
 		{
 			st = 0;
 			while (av[st])
@@ -112,33 +131,30 @@ void	exec_cmds(char **av, char **env)
 				}
 				st++;
 			}
-			/*i = 0;
-			while (av[i])
-			{
-				printf("%s\n",av[i]);
-				i++;
-			}	
-			printf("----------\n");
-			exit(0);*/
 			if (j > 0)
-                        {
-                                dup2(prev[0], 0);
-                                close(prev[0]);
-                                close(prev[1]);
-                        }
-                        if (j < count - 1)
-                        {
-                                dup2(next[1], 1);
-                                close(next[0]);
-                                close(next[1]);
-                        }
+			{
+				dup2(prev[0], 0);
+				close(prev[0]);
+				close(prev[1]);
+			}
+			if (j < count - 1)
+			{
+				dup2(next[1], 1);
+				close(next[0]);
+				close(next[1]);
+			}
 			if (!strcmp(av[0], "cd"))
 			{
 				chdir(av[1]);
 				exit(0);
 			}
-			execve(av[0], av, env);
-			exit(0);
+			if (execve(av[0], av, env))
+			{
+				print_error("error: cannot execute ");
+				print_error(av[0]);
+				print_error("\n");
+				exit(1);
+			}
 		}
 		else
 		{
@@ -151,53 +167,28 @@ void	exec_cmds(char **av, char **env)
 				}
 				av++;
 			}
-			reset(prev, next, j ,count);
+			reset(prev, next, j, count);
 			j++;
 		}
 	}
-		
-	/*while (str[i])
-	{
-		if (str[i][0] == '|')
-			count++;
-		i++;
-	}
 	i = -1;
 	while (++i < count)
-	{
-		if (i < count - 1)
-			pipe(next);
-		pid = fork();
-		if (pid < 0)
-			exit(0);
-		if (pid == 0)
-		{
-			if (i > 0)
-			{
-				dup2(prev[0], 0);
-				close(prev[0]);
-				close(prev[1]);
-			}
-			if (i < count - 1)
-			{
-				dup2(next[1], 1);
-				close(next[0]);
-				close(next[1]);
-			}
-			execve(str[0], str, env);
-		}
-		reset(prev, next);
-	}	*/
+		waitpid(pid[i], &exit_status, 0);
+	return (exit_status >> 8);
 }
 
-int		main(int ac, char **av, char **env)
+
+int	main(int ac, char **av, char **env)
 {
 	int	i;
+	int	status;
 
+	(void)ac;
+	status = 0;
 	i = 1;
 	while (av[i])
 	{
-		exec_cmds(&av[i], env);
+		status = exec_cmds(&av[i], env);
 		while (av[i])
 		{
 			if (av[i] && av[i][0] == ';')
@@ -208,5 +199,5 @@ int		main(int ac, char **av, char **env)
 			i++;
 		}
 	}
-	return 0;
+	return (status);
 }
